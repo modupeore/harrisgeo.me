ea7a90d9e007f6572eacf8766d010e2027771196
harrisgeo88 <charilaoss@gmail.com>
2019-04-01 22:35:43 +0100

    Temporary fix for main image

--- src/components/layout/layout.js
+++ src/components/layout/layout.js
@@ -2,7 +2,11 @@ import React from "react"
 import { Navbar } from "../navbar"
 import { Body, Image } from "./"
 
-export const Img = props => <Image {...props} />
+export const Img = props => (
+  <div>
+    <Image {...props} />
+  </div>
+)
 
 export const Layout = ({ children, isLandingPage = true }) => (
   <Body>
