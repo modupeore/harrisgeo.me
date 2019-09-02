5898b9792d434ea7d6ba0323d9550f513140c230
Zander Martineau <mrmartineau@users.noreply.github.com>
2019-09-02 12:01:38 +0100

    fix your buggy code

--- src/pages/main.js
+++ src/pages/main.js
@@ -16,23 +16,21 @@ const Main = () => (
     <Helmet title="Harris Geo" defer={false} />
     <H1 id="home">HARRIS GEO</H1>
     <Wrapper>
-      <Img src={MeSnow} />
+      <Img src={MeSnow} alt="Harris in the snow" />
       <Bio>
-        I am a Full stack developer. I have been working with <b>React</b>
-        ,&nbsp; <b>Node.js</b> and&nbsp;
-        <b>Typescript </b> for the last 6+ years.
+        I am a full-stack developer. I have been working with <b>React</b>, <b>Node.js</b> and <b>TypeScript</b> for the last 6+ years.
         <Box>
           Now working @&nbsp;
-          <Link href="https://www.fairfx.com/" target="_blank">
-            FairFX
+          <Link href="https://equals.co">
+            Equals
           </Link>
         </Box>
         <Box>
-          <Link href="https://github.com/harrisgeo88" target="_blank">
+          <Link href="https://github.com/harrisgeo88">
             Github
           </Link>
           &nbsp;-&nbsp;
-          <Link href="https://twitter.com/harrisgeo88" target="_blank">
+          <Link href="https://twitter.com/harrisgeo88">
             Twitter
           </Link>
         </Box>
