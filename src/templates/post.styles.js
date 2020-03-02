70f7b752c35fd32d070e7e3ced542571d3f4d0ad
harrisgeo88 <harrisgeo1988@gmail.com>
2020-03-02 22:13:05 +0000

    Tiny theme changes

--- src/templates/post.styles.js
+++ src/templates/post.styles.js
@@ -1,7 +1,6 @@
 import { H1 as H1Layout } from "../components/layout"
-import { white, dark, hover } from '../components/tokens'
+import { white, dark, hover, black } from '../components/tokens'
 import styled from "styled-components"
-
 const font = "'Merriweather', sans-serif"
 
 
@@ -14,8 +13,13 @@ export const PostContainer = styled.div`
   }
 
   > div > pre {
-    background: ${white};
+    margin: 20px; padding: 20px;
+    color: red;
+    background-color: ${dark};
+    white-space: pre; text-shadow: 0 1px 0 ${black};
+    font: 16px/24px 'Courier New', Courier, 'Lucida Sans Typewriter', 'Lucida Typewriter', monospace;
   }
+
 
   > ol > li > a, > p > a {
     color: ${props => props.dark ? white : dark};
