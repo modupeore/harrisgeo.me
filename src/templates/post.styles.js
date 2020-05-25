9f33e19006e50260e498a03c34da5549349799ff
Harris Geo <charilaoss@gmail.com>
2020-05-25 21:16:38 +0100

    centered images in blogs and command cleanup

--- src/templates/post.styles.js
+++ src/templates/post.styles.js
@@ -1,31 +1,41 @@
 import { H1 as H1Layout } from "../components/layout"
-import { white, dark, hover, black } from '../components/tokens'
+import { white, dark, hover, black, mobile } from "../components/tokens"
 import styled from "styled-components"
 const font = "'Merriweather', sans-serif"
-
 
 export const PostContainer = styled.div`
   margin: 50px 0;
   font-family: ${font};
-  line-height: 2.0;
-  > p > code, ol > li > code {
+  line-height: 2;
+  > p > code,
+  ol > li > code {
     background: none;
   }
 
   > div > pre {
-    margin: 20px; padding: 20px;
+    margin: 20px;
+    padding: 20px;
     color: red;
     background-color: ${dark};
-    white-space: pre; text-shadow: 0 1px 0 ${black};
-    font: 16px/24px 'Courier New', Courier, 'Lucida Sans Typewriter', 'Lucida Typewriter', monospace;
+    white-space: pre;
+    text-shadow: 0 1px 0 ${black};
+    font: 16px/24px "Courier New", Courier, "Lucida Sans Typewriter",
+      "Lucida Typewriter", monospace;
   }
 
-
-  > ol > li > a, > p > a {
-    color: ${props => props.dark ? white : dark};
+  > ol > li > a,
+  > p > a {
+    color: ${(props) => (props.dark ? white : dark)};
     font-weight: 600;
     :hover {
       color: ${hover};
+    }
+  }
+
+  @media screen and (max-width: ${mobile}) {
+    img {
+      width: 75%;
+      height: 75%;
     }
   }
 `
